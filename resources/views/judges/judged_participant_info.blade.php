44444de95472cf2dbdba7b546b108f60fc27eb2b
Ali Hassaan Mughal <amughal.bscs16seecs@seecs.edu.pk>
2020-06-21 07:44:06 +0500

    ..

--- resources/views/judges/judged_participant_info.blade.php
+++ resources/views/judges/judged_participant_info.blade.php
@@ -63,6 +63,173 @@
 								<td>{{$team->Q6}}/10</td>
 								<td>{{$team->comments_q6}}</td>
 							</tr>
+
+
+							{{--Judge # 2--}}
+                            <?php } else if ($team->judge_id_2==$judge){ ?>
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$team->Q1_2}}/10</td>
+								<td>{{$team->comments_q1_2}}</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>{{$team->Q2_2}}/10</td>
+								<td>{{$team->comments_q2_2}}</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>{{$team->Q3_2}}/10</td>
+								<td>{{$team->comments_q3_2}}</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>{{$team->Q4_2}}/10</td>
+								<td>{{$team->comments_q4_2}}</td>
+							</tr>
+							<tr>
+								<th scope="row">5</th>
+								<td>{{$team->Q5_2}}/10</td>
+								<td>{{$team->comments_q5_2}}</td>
+							</tr>
+							<tr>
+								<th scope="row">6</th>
+								<td>{{$team->Q6_2}}/10</td>
+								<td>{{$team->comments_q6_2}}</td>
+							</tr>
+
+							{{--Judge # 3--}}
+                            <?php } else if ($team->judge_id_3==$judge){ ?>
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$team->Q1_3}}/10</td>
+								<td>{{$team->comments_q1_3}}</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>{{$team->Q2_3}}/10</td>
+								<td>{{$team->comments_q2_3}}</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>{{$team->Q3_3}}/10</td>
+								<td>{{$team->comments_q3_3}}</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>{{$team->Q4_3}}/10</td>
+								<td>{{$team->comments_q4_3}}</td>
+							</tr>
+							<tr>
+								<th scope="row">5</th>
+								<td>{{$team->Q5_3}}/10</td>
+								<td>{{$team->comments_q5_3}}</td>
+							</tr>
+							<tr>
+								<th scope="row">6</th>
+								<td>{{$team->Q6_3}}/10</td>
+								<td>{{$team->comments_q6_3}}</td>
+							</tr>
+
+							{{--Judge # 4--}}
+                            <?php } else if ($team->judge_id_4==$judge){ ?>
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$team->Q1_4}}/10</td>
+								<td>{{$team->comments_q1_4}}</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>{{$team->Q2_4}}/10</td>
+								<td>{{$team->comments_q2_4}}</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>{{$team->Q3_4}}/10</td>
+								<td>{{$team->comments_q3_4}}</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>{{$team->Q4_4}}/10</td>
+								<td>{{$team->comments_q4_4}}</td>
+							</tr>
+							<tr>
+								<th scope="row">5</th>
+								<td>{{$team->Q5_4}}/10</td>
+								<td>{{$team->comments_q5_4}}</td>
+							</tr>
+							<tr>
+								<th scope="row">6</th>
+								<td>{{$team->Q6_4}}/10</td>
+								<td>{{$team->comments_q6_4}}</td>
+							</tr>
+
+							{{--Judge # 5--}}
+                            <?php } else if ($team->judge_id_5==$judge){ ?>
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$team->Q1_5}}/10</td>
+								<td>{{$team->comments_q1_5}}</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>{{$team->Q2_5}}/10</td>
+								<td>{{$team->comments_q2_5}}</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>{{$team->Q3_5}}/10</td>
+								<td>{{$team->comments_q3_5}}</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>{{$team->Q4_5}}/10</td>
+								<td>{{$team->comments_q4_5}}</td>
+							</tr>
+							<tr>
+								<th scope="row">5</th>
+								<td>{{$team->Q5_5}}/10</td>
+								<td>{{$team->comments_q5_5}}</td>
+							</tr>
+							<tr>
+								<th scope="row">6</th>
+								<td>{{$team->Q6_5}}/10</td>
+								<td>{{$team->comments_q6_5}}</td>
+							</tr>
+
+							{{--Judge # 6--}}
+							<?php } else {?>
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$team->Q1_6}}/10</td>
+								<td>{{$team->comments_q1_6}}</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>{{$team->Q2_6}}/10</td>
+								<td>{{$team->comments_q2_6}}</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>{{$team->Q3_6}}/10</td>
+								<td>{{$team->comments_q3_6}}</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>{{$team->Q4_6}}/10</td>
+								<td>{{$team->comments_q4_6}}</td>
+							</tr>
+							<tr>
+								<th scope="row">5</th>
+								<td>{{$team->Q5_6}}/10</td>
+								<td>{{$team->comments_q5_6}}</td>
+							</tr>
+							<tr>
+								<th scope="row">6</th>
+								<td>{{$team->Q6_6}}/10</td>
+								<td>{{$team->comments_q6_6}}</td>
+							</tr>
+							<?php } ?>
 							</tbody>
 						</table>
 
